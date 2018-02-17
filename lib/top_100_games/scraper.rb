2d61dc1c0fd15a9b29f4912e3d16efc897f8326d
Ahmad <github email address>
2018-02-16 19:18:43 -0800

    decide on approach of classes.  Scrape all necessary attributes

--- lib/top_100_games/scraper.rb
+++ lib/top_100_games/scraper.rb
@@ -6,14 +6,19 @@ class Top100Games::Scraper
 		Nokogiri::HTML(open("https://gamefaqs.gamespot.com/games/topgamesrollup"))
 	end
 
-	#scrape the website to get names and attribute of top games
+	#scrape the website to get names and top level attributes of top games
 	def scrape_top_games_index
 		self.get_page.css(".pod_topgames ol li")
+		rank = items.css("div.count").text
+		name = items.css("span.name a").text
+		consoles = items.css("span.sub").text
+		url = "https://gamefaqs.gamespot.com#{items.css("a").attribute("href").text}"
 	end
 
+	#scrape individual game's attributes
 	def create_game_profile
-		scrape_top_games_index.each { |game| Top100Games::Game.create_from_index(game)}
-			binding.pry
+		
+			
 	end
 
 
